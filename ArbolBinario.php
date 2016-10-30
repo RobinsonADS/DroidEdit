2587c37ed28468c348680bcb4ea7170b0d50c5c0
Robinson De La Cruz <rodec2506@gmail.com>
2016-10-29 20:55:25 -0500

    Correccion de algunos metodos

--- ArbolBinario.php
+++ ArbolBinario.php
@@ -7,12 +7,45 @@ class ArbolBinario{
     $this->raiz = $root;
   }
 
-  public function agregarNodo($nombrePapa, $ubicacion, $nombreHijo){
-    if($ubicacion == "derecha"){
-      $nombrePapa->setDerecha($nombreHijo);
+  public function getRaiz(){
+    return $this->raiz;
+  }
+
+  public function getNodo($nodo, $info){
+    if($nodo!=null){
+      if($info == $nodo->getInfo()){
+         return $nodo;
+      }else{
+        if($nodo->getDerecha()!=null){
+          if($this->getNodo($nodo->getDerecha(), $info)==null){
+              return $this->getNodo($nodo->getIzquierda(), $info);
+          }
+        }
+        if($nodo->getIzquierda()!=null){
+          if($this->getNodo($nodo->getIzquierda(), $info)==null){
+              return $this->getNodo($nodo->getDerecha(), $info);
+          }
+        }
+      }
+    }else{
+        return null;
     }
-    if($ubicacion == "izquierda"){
-      $nombrePapa->setIzquierda($nombreHijo);
+  }
+
+  public function agregarNodo($papa, $ubicacion, $nombreHijo){
+    //para saber si el nodo a agregar ya existe en el arbol
+    $existe = $this->getNodo($this->raiz, $nombreHijo->getInfo());
+    //para obtener el nodo papa con la info agregada
+    $nombrePapa = $this->getNodo($this->raiz, $papa);
+    if($existe == null){
+      if($ubicacion == "derecha"){
+        $nombrePapa->setDerecha($nombreHijo);
+      }
+      if($ubicacion == "izquierda"){
+        $nombrePapa->setIzquierda($nombreHijo);
+      }
+    }else{
+      return "No se pudo agregar";
     }
   }
 
@@ -30,32 +63,51 @@ class ArbolBinario{
     return false;
   }
 
+  //Metodo para allar el nodo padre de un nodo dado
   public function getPadre($nodo, $info){
-    if($nodo->getDerecha() == $info || $nodo->getIzquierda() == $info){
-      return $nodo;
-    }else{
-      if($this->getPadre($nodo->getDerecha())!=null){
-        return $this->getPadre($nodo->getDerecha());
+    if($nodo!=null){
+      if($nodo->getDerecha()!=null){
+        if($nodo->getDerecha()->getInfo() == $info){
+          return $nodo;
+        }else{
+          if($this->getPadre($nodo->getDerecha(), $info)==null){
+            return $this->getPadre($nodo->getIzquierda(), $info);
+          }
+        }
       }
-      if($this->getPadre($nodo->getIzquierda())!=null){
-        return $this->getPadre($nodo->getIzquierda());
+      if($nodo->getIzquierda()!=null){
+        if($nodo->getIzquierda()->getInfo() == $info){
+          return $nodo;
+        }else{
+          if($this->getPadre($nodo->getIzquierda(), $info)==null){
+            return $this->getPadre($nodo->getDerecha(), $info);
+          }
+        }
       }
+    }else {
+      return null;
     }
-    return null;
   }
 
-  public function eliminarNodo($nodoEliminar){
-    if($this->esHoja($nodoEliminar)){
-      $padre = $this->getPadre($nodoEliminar);
-      if($padre->getDerecha() == $nodoEliminar){
-        $padre->setDerecha(null);
+  public function eliminarNodo($nodo){
+    $nodoEliminar = $this->getNodo($this->raiz, $nodo);
+    if($nodoEliminar!=null){
+      if($this->esHoja($nodoEliminar)){
+        $padre = $this->getPadre($this->raiz, $nodoEliminar->getInfo());
+        if($padre->getDerecha()->getInfo() == $nodoEliminar->getInfo()){
+          $padre->setDerecha(null);
+          return true;
+        }
+        if($padre->getIzquierda()->getInfo() == $nodoEliminar->getInfo()){
+          $padre->setIzquierda(null);
+          return true;
+        }
       }
-      if($padre->getIzquierda() == $nodoEliminar){
-      $padre->setIzquierda(null);
-      }
+    }else{
+      return false;
     }
   }
-  
+
   public function contarNodos($nodo){
     if($nodo != null){
       return $this->contarNodos($nodo->getDerecha())+$this->contarNodos($nodo->getIzquierda())+1;
@@ -63,7 +115,7 @@ class ArbolBinario{
            return 0;
          }
   }
-  
+
   public function contarNumerosPares($nodo){
     if($nodo != null){
       if($nodo->getInfo()%2==0){
@@ -75,7 +127,7 @@ class ArbolBinario{
             return 0;
          }
   }
-  
+
   public function altura($nodo){
     if($nodo!=null){
       if($nodo->getDerecha()!=null){
@@ -84,24 +136,24 @@ class ArbolBinario{
           return $this->altura($nodo->getDerecha())+$this->altura($nodo->getIzquierda());
         }else{
                 return 0;
-             }
+        }
       }else{
               return 0;
-           }
+      }
        if($nodo->getIzquierda()!=null){
         return $this->altura($nodo->getDerecha())+$this->altura($nodo->getIzquierda())+1;
         if($nodo->getDerecha()!=null){
           return $this->altura($nodo->getDerecha())+$this->altura($nodo->getIzquierda());
         }else{
                 return 0;
-             }
+        }
       }else{
               return 0;
-           }
+      }
     }else{
-            return 0;  
-         }
-  
+            return 0;
+    }
+
   }
 
 }
--- pruebas.php
+++ pruebas.php
@@ -1,11 +1,23 @@
 <?php
 include ("ArbolBinario.php");
 
-  $arbol = new ArbolBinario(5);
-  
-  $arbol->agregarNodo(5, "derecha", 3);
-  
-  echo $arbol->getDerecha();
-  
+  $arbol = new ArbolBinario(new NodoBinario(5));
+
+  //Prueba de agregar los nodos
+  $arbol->agregarNodo(5, "izquierda", new NodoBinario(3));
+  $arbol->agregarNodo(5, "derecha", new NodoBinario(4));
+  $arbol->agregarNodo(3, "izquierda", new NodoBinario(6));
+  $arbol->agregarNodo(3, "derecha", new NodoBinario(7));
+
+  //Prueba eliminar nodo
+  if($arbol->eliminarNodo(4)){
+    echo "true";
+  }else{
+    echo "false";
+  }
+
+  echo "<hr>";
+  print_r($arbol);
+
 
 ?>
